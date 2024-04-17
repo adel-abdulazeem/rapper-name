5dab1d61073142dfd61f2c1816108de611ccb8c2
Adel Abdulazeem <adelnasr029@gmail.com>
2024-04-17 18:57:26 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age': 30,
+        'birthName':'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance rhe rapper': {
+        'age': 28,
+        'birthName':'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'dylan': {
+        'age': 27,
+        'birthName':'Dylan',
+        'birthLocation': 'Dylan'
+    },
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (req, res) => {
+    const rappersName = req.params.rapperName.toLowerCase()
+    if(rappers[rappersName]){
+        res.json(rappers[rappersName])
+    }else{
+        res.json(rappers['dylan'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port: ${PORT}`)
+})
